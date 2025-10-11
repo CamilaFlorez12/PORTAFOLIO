4557bcf01498650bc5d41559b1b86fdb4cc9dca3
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-11 14:33:03 -0500

    feat: :tada: initial portafolio
    initial protafolio
    
    By@CamilaFlorez

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar/navbar.jsx';
+import Presentation from './components/Presentation/presentation.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Presentation/>
+    </div>
+  )
+}
+
+export default App
