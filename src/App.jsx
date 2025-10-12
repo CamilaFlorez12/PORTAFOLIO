06ab9b94ce2c9c68266a15f08f5da067249f35d6
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-12 15:45:21 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; 
 import Navbar from "./components/Navbar/navbar.jsx";
 import Presentation from "./components/Presentation/presentation.jsx";
 import AboutMe from "./components/About-me/About.jsx";
@@ -9,7 +9,7 @@ import Contact from "./components/Contact/contact.jsx";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Presentation />} />
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/Skills" element={<Skills />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
