e328ebb7c30dae973a7b2695bf30e963e3881ee2
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-12 15:40:49 -0500

    fix: :bug: change routes
    change routes
    
    By@Camilaflorez

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar.jsx";
 import Presentation from "./components/Presentation/presentation.jsx";
 import AboutMe from "./components/About-me/About.jsx";
