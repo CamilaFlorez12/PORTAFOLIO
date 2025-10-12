c3613a9b225e0b27080936bfbf49006e6cdd6293
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-11 20:50:07 -0500

    fix: :bug: change conection
    change conection{
    
    By@CamilaFlorez

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar/navbar.jsx';
 import Presentation from './components/Presentation/presentation.jsx'
+import AboutMe from './components/About-me/About.jsx';
+import Projects from './components/projects/project.jsx';
+import Skills from './components/skillCart/skill.jsx';
+import Contact from './components/Contact/contact.jsx';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Presentation/>
+      <AboutMe/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
     </div>
   )
 }
