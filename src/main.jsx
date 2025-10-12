661d9a968e6c8ff2be8ab5cd41211057c0b75e62
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-12 17:18:08 -0500

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css'
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
