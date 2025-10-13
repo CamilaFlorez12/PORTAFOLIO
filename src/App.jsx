4272c952da142effa856a5644fd207b89487515e
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-10-12 20:25:33 -0500

    despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom"; 
 import Navbar from "./components/Navbar/navbar.jsx";
 import Presentation from "./components/Presentation/presentation.jsx";
-import AboutMe from "./components/About-me/About.jsx";
+import AboutMe from "./components/About-me/about.jsx";
 import Projects from "./components/projects/project.jsx";
 import Skills from "./components/skillCart/skill.jsx";
 import Contact from "./components/Contact/contact.jsx";
